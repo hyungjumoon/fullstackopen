c8a150141e15d0d68bf88a626d78b74bed573700
Hector <hyungjumoon@gmail.com>
2024-11-07 17:48:33 -0500

    exercises 5.5-5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button><br />
+        {blog.url} <br />
+        {blog.likes} <button onClick={(event) => event.preventDefault}>like</button> <br />
+        {blog.user ? blog.user.name : 'no user, malformed blog object'}
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
