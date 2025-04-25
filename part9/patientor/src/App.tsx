c929180b630aa34faaa04b098fd4ec8d6dc56df4
Hector <hyungjumoon@gmail.com>
2025-04-25 14:19:52 -0400

    exercise 9.22 full

--- part9/patientor/src/App.tsx
+++ part9/patientor/src/App.tsx
@@ -36,7 +36,7 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
-            <Route path="/patients/:id" element={<PatientView patients={patients} />} />
+            <Route path="/patients/:id" element={<PatientView />} />
           </Routes>
         </Container>
       </Router>
--- part9/patientor/src/components/PatientView.tsx
+++ part9/patientor/src/components/PatientView.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
+import patientService from "../services/patients";
 import { useParams } from "react-router-dom";
 import { Patient } from "../types";
 
-interface Props {
-  patients : Patient[]
-}
+const PatientView = () => {
+  const [patient, setPatient] = useState<Patient>();
+  let id = useParams().id;
+  if (id === undefined) {
+    id = "";
+  }
 
-const PatientView = ({ patients } : Props) => {
-  const id = useParams().id;
-  const patient: Patient | undefined = patients.find(a => a.id === id);
+  useEffect(() => {
+    const fetchPatient = async () => {
+      const tmp = await patientService.getOne(id);
+      setPatient(tmp);
+    };
+    void fetchPatient();
+  }, [id]);
+
+  // const patient: Patient | undefined = patients.find(a => a.id === id);
   if (patient === undefined) {
     return (
       <div>
@@ -19,6 +30,7 @@ const PatientView = ({ patients } : Props) => {
     <div>
       <h2>{patient.name} </h2>
       <div>
+        gender: {patient.gender} <br />
         ssh: {patient.ssn} <br />
         occupation: {patient.occupation}
       </div>
